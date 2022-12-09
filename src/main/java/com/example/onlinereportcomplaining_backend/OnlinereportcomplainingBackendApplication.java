78733b4a2f48781162c7d367a962351d7dcd18d3
HarshaSalih <harshasalih2000@gmail.com>
2022-12-09 14:29:29 +0530

    commits

---
+++ src/main/java/com/example/onlinereportcomplaining_backend/OnlinereportcomplainingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinereportcomplaining_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinereportcomplainingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinereportcomplainingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlinereportcomplaining_backend/OnlinereportcomplainingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinereportcomplaining_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinereportcomplainingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
