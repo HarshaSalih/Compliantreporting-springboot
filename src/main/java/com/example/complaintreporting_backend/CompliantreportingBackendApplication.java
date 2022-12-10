ee127fdc8bfd0d4f001353724bed8f53c532df8d
HarshaSalih <harshasalih2000@gmail.com>
2022-12-11 03:07:21 +0530

    commits

--- src/main/java/com/example/complaintreporting_backend/CompliantreportingBackendApplication.java
+++ src/main/java/com/example/complaintreporting_backend/CompliantreportingBackendApplication.java
@@ -1,13 +1,13 @@
-package com.example.onlinereportcomplaining_backend;
+package com.example.complaintreporting_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnlinereportcomplainingBackendApplication {
+public class CompliantreportingBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnlinereportcomplainingBackendApplication.class, args);
+		SpringApplication.run(CompliantreportingBackendApplication.class, args);
 	}
 
 }
--- src/main/java/com/example/complaintreporting_backend/dao/UserDao.java
+++ src/main/java/com/example/complaintreporting_backend/dao/UserDao.java
@@ -1,17 +1,20 @@
-package com.example.onlinereportcomplaining_backend.dao;
+package com.example.complaintreporting_backend.dao;
 
-import com.example.onlinereportcomplaining_backend.model.OnlineReportComplaining;
+import com.example.complaintreporting_backend.model.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface OnlineReportComplainingDao extends CrudRepository<OnlineReportComplaining,Integer> {
+public interface UserDao extends CrudRepository<User,Integer> {
 
     @Query(value = "SELECT `id`, `address`, `email`, `name`, `password`, `phone`, `username` FROM `online_report_complaining` WHERE `username`= :username AND `password`= :password",nativeQuery = true)
-    List<OnlineReportComplaining> userLogin(@Param("username")String username,@Param("password")String password);
+    List<User> userLogin(@Param("username")String username, @Param("password")String password);
 
     @Query(value = "SELECT `id`, `address`, `email`, `name`, `password`, `phone`, `username` FROM `online_report_complaining` WHERE `username`= :username",nativeQuery = true)
-    List<OnlineReportComplaining> FindUser(@Param("username")String username);
+    List<User> FindUser(@Param("username")String username);
+
+    @Query(value = "SELECT `id`, `address`, `email`, `name`, `password`, `phone`, `username` FROM `online_report_complaining` WHERE `id`= :id",nativeQuery = true)
+    List<User>viewProfile(@Param("id")Integer id);
 }
